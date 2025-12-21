b2cb7e08c0ecfc56090b2add39e84ae4ed00c388
Toy <1206256980@qq.com>
2025-12-21 08:48:21 +0800

    jvm调整为2g

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# 启动后端 (后台运行) - JVM堆内存1G
-echo "Starting backend with 1G heap..."
-java -Xms512m -Xmx1024m -jar /app/app.jar &
+# 启动后端 (后台运行) - JVM堆内存2G
+echo "Starting backend with 2G heap..."
+java -Xms512m -Xmx2048m -jar /app/app.jar &
 
 # 等待后端启动
 sleep 5
