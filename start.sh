39c73d3677efa36b4e658c25663da51602bc6539
ex_ouyt3 <ex_ouyt3@partner.midea.com>
2025-12-24 18:00:42 +0800

    jvm调成8g

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# 启动后端 (后台运行) - JVM堆内存2G
-echo "Starting backend with 2G heap..."
-java -Xms512m -Xmx2048m -jar /app/app.jar &
+# 启动后端 (后台运行) - JVM堆内存6G
+echo "Starting backend with 6G heap..."
+java -Xms2g -Xmx6g -jar /app/app.jar &
 
 # 等待后端启动
 sleep 5
